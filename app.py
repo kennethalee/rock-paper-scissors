2db374712dbe081b566fdc9f464be0e8471880da
Kenneth Aidan Lee <kennethlee@Kenneths-MBP.lan>
2024-10-27 00:15:39 +0900

    code corrections for page redirect

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
@@ -15,9 +14,34 @@ db.init_app(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+@app.route('/')
+def home():
+    if not User.query.first():
+        return redirect(url_for('register'))
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    return render_template('index.html')
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Logged in successfully!')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Check your username and password')
+
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -31,29 +55,15 @@ def register():
             return redirect(url_for('register'))
 
         # Hash password and create new username
-        hashed_password = generate_password_hash(password, method='sha256')
-        new_user = User(username=username, password_1=hashed_password)
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
+        flash('Registration complete! Log in!')
+        return redirect(url_for('login'))
+
     return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        user = User.query.filter_by(username=username).first()
-
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            flash('Logged in successfully!')
-            return redirect(url_for('profile'))
-        else:
-            flash('Login failed. Check your username and password')
-
-    return render_template('login.html')
 
 @app.route('/profile')
 @login_required
@@ -68,3 +78,30 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('login'))
+
+@app.route('/play', methods=['POST'])
+@login_required
+def play():
+    player_choice = request.form.get('choice')
+    computer_choice = main.get_computer_choice()
+    result = main.determine_winner(player_choice, computer_choice)
+
+    # Save result to database
+    new_game = Game(
+        user_id=current_user.id,
+        player_choice=player_choice,
+        computer_choice=computer_choice,
+        result=result
+    )
+    db.session.add(new_game)
+    db.session.commit()
+
+    return render_template(
+        'result.html', player_choice=player_choice,
+        computer_choice=computer_choice, result=result
+    )
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
