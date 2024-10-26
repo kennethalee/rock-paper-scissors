2531e234f5687db34b0f22e97e1c6794b162565a
Kenneth Aidan Lee <kennethlee@Kenneths-MBP.lan>
2024-10-26 22:41:17 +0900

    modified game logic

--- main.py
+++ main.py
@@ -1,45 +1,18 @@
-from flask import Flask, render_template, request
 import random
 
-app = Flask(__name__)
+def get_computer_choice():
+    """Generate random choice for computer"""
+    choices = ['rock', 'paper', 'scissors']
+    return random.choice(choices)
 
-choices = ['rock', 'paper', 'scissors']
-
-def determine_winner(player, computer):
-    """
-    Determine the winner based on player and computer choices
-    returning win, lose or tie
-    :param player:
-    :param computer:
-    :return:
-    """
-    if player == computer:
+def determine_winner(player_choice, computer_choice):
+    """Determine the game result based on player and computer choice"""
+    if player_choice == computer_choice:
         return "It's a Tie!"
     elif (
-        player == 'rock' and computer == 'scissors' or
-        player == 'paper' and computer == 'rock' or
-        player == 'scissor' and computer == 'paper'
+            (player_choice == 'rock' and computer_choice == 'scissors') or
+            (player_choice == 'scissors' and computer_choice == 'paper') or
+            (player_choice == 'paper' and computer_choice == 'rock')
     ): return 'You Win!'
     else:
         return 'You Lose :('
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/play', methods=['POST'])
-def play():
-    """
-    Play single round and return result
-    :return:
-    """
-    player_choice = request.form['choice']
-    computer_choice = random.choice(choices)
-
-    result = determine_winner(player_choice, computer_choice)
-
-    return render_template('result.html', player_choice=player_choice,
-                           computer_choice=computer_choice, result=result)
-
-if __name__ == '__main__':
-    app.run(debug=True)
